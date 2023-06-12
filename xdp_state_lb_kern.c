d36c0275819ab799f477cfa82cf6bd8298427b6b
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-06-12 16:45:15 +0800

    Update xdp_state_lb_kern.c

--- xdp_state_lb_kern.c
+++ xdp_state_lb_kern.c
@@ -108,9 +108,9 @@ int xdp_state_load_balancer(struct xdp_md *ctx) {
             /* bpf_printk("Calling fib_params_redirect ...");
             return bpf_redirect(fib_params.ifindex, 0); */
             
-            bpf_printk("eth->h_dest[5] = %x", eth->h_dest[5]);
-            bpf_printk("eth->source[5] = %x", eth->h_source[5]);
-            bpf_printk("Returning action XDP_TX ...");
+            bpf_printk("Before XDP_TX, iph->saddr = %x, iph->daddr = %x", iph->saddr, iph->saddr);
+            bpf_printk("Before XDP_TX, eth->h_source[5] = %x, eth->dest[5] = %x", eth->h_source[5], eth->dest[5]);
+            bpf_printk("Returning XDP_TX ...");
             return XDP_TX;
             
         case BPF_FIB_LKUP_RET_BLACKHOLE:
